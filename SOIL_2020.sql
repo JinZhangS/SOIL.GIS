a5b10f56444d6519e7cbd435cfaece84cef8ef70
JinZhangS <48298619+JinZhangS@users.noreply.github.com>
2021-02-23 22:51:10 -0800

    Delete SOIL_2020.sql
    Add into the wrong repository

--- SOIL_2020.sql
+++
@@ -1,7 +0,0 @@
-CREATE DATABASE soil_2020;
-USE soil_2020;
-CREATE USER 'jza18'@'localhost' IDENTIFIED BY 'sfusoil2021';
-grant select, insert, update, delete, create, execute, show view on soil_2020.* to'jza18'@'localhost';
-alter user 'jza18'@'localhost' identified with mysql_native_password by'sfusoil2021';
-
-
